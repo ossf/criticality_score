b64d0a01ee87a6a077b7e529fe0857a1780a4ff4
Abhishek Arya <inferno@chromium.org>
2020-11-17 08:15:45 -0800

    First commit.

---
+++ README.md
@@ -0,0 +1,87 @@
+# Open Source Project Criticality Score
+
+## Goals
+1. Generate a **criticality score** for every open source project.
+
+1. Create a list of critical projects that the open source community depends on.
+
+1. Use this data to proactively improve the security posture of these critical projects.
+
+## Usage
+
+The program only requires one argument to run, the name of the repo:
+
+```shell
+$ pip3 install .
+$ criticality_score --repo github.com/kubernetes/kubernetes
+name: kubernetes
+url: https://github.com/kubernetes/kubernetes
+language: Go
+created_since: 78
+updated_since: 0
+contributor_count: 3649
+org_count: 5
+commit_frequency: 105.9
+recent_releases_count: 77
+closed_issues_count: 3333
+updated_issues_count: 5489
+comment_frequency: 5.4
+dependents_count: 403529
+criticality_score: 0.98514
+```
+
+A project's criticality score is a number between 0 and 1. It is based on
+the following algorithm:
+
+<img src="images/formula.png" width="359" height="96">
+
+It is derived using the following parameters:
+
+| Parameter(Si)  | Weight (ai) | Max threshold(Ti) | Description |
+|---|---|---|---|
+| created_since | 1 | 120 | Time since the project was created (in months) |
+| updated_since  | -1 | 120 | Time since the project was last updated (in months) |
+| contributor_count | 2 | 5000 | Count of project contributors (with commits) |
+| org_count | 1 | 10 | Count of distinct organizations that contributors belong to |
+| commit_frequency | 1 | 1000 | Average number of commits per week in the last year |
+| recent_releases_count | 0.5 | 26 | Number of releases in the last year |
+| closed_issues_count | 0.5 | 5000 | Number of issues closed in the last 90 days |
+| updated_issues_count | 0.5 | 5000 | Number of issues updated in the last 90 days |
+| comment_frequency | 1 | 15 | Average number of comments per issue in the last 90 days |
+| dependents_count | 2 | 500000 | Number of project mentions in the commit messages |
+
+### Authentication
+
+Before running criticality score, you need to
+[create a GitHub access token](https://docs.github.com/en/free-pro-team@latest/developers/apps/about-apps#personal-access-tokens)
+and set it in environment variable `GITHUB_AUTH_TOKEN`.
+This helps to avoid the GitHub's
+[api rate limits](https://developer.github.com/v3/#rate-limiting)
+with unauthenticated requests.
+
+```shell
+# For posix platforms, e.g. linux, mac:
+export GITHUB_AUTH_TOKEN=<your access token>
+
+# For windows:
+set GITHUB_AUTH_TOKEN=<your access token>
+```
+
+As an alternative to personal access tokens, we also support GitHub App Installations
+for higher rate-limit quotas.
+If you have an installed GitHub App and key file, you can use these three environment
+variables, following the commands shown above for your platform.
+
+```
+GITHUB_APP_KEY_PATH=<path to the key file on disk>
+GITHUB_APP_INSTALLATION_ID=<installation id>
+GITHUB_APP_ID=<app id>
+```
+
+These can be obtained from the GitHub [developer settings](https://github.com/settings/apps) page.
+
+### Formatting Results
+
+There are three formats currently: `default`, `json`, and `csv`. Others may be added in the future.
+
+These may be specified with the `--format` flag.
---
+++ criticality_score/run.py
@@ -0,0 +1,274 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Main python script for calculating OSS Criticality Score."""
+
+import argparse
+import csv
+import datetime
+import json
+import math
+import os
+import requests
+import sys
+import time
+import urllib
+
+import github
+
+from .constants import *
+
+
+class Repository:
+    def __init__(self, repo):
+        self._repo = repo
+
+
+class GitHubRepository(Repository):
+    # General metadata attributes.
+    @property
+    def name(self):
+        return self._repo.name
+
+    @property
+    def url(self):
+        return self._repo.html_url
+
+    @property
+    def language(self):
+        return self._repo.language
+
+    # Criteria important for ranking.
+    @property
+    def created_since(self):
+        difference = datetime.datetime.utcnow() - self._repo.created_at
+        return round(difference.days / 30)
+
+    @property
+    def updated_since(self):
+        last_commit = self._repo.get_commits()[0]
+        last_commit_time = last_commit.commit.author.date
+        difference = datetime.datetime.utcnow() - last_commit_time
+        return round(difference.days / 30)
+
+    @property
+    def contributors(self):
+        try:
+            return self._repo.get_contributors(anon='true').totalCount
+        except Exception:
+            # Very large number of contributors, i.e. 5000+. Cap at 5,000.
+            return 5000
+
+    def get_contributor_orgs(self):
+        def _filter_name(org_name):
+            return org_name.lower().replace('inc.', '').replace(
+                'llc', '').replace('@', '').replace(' ', '').rstrip(',')
+
+        orgs = set()
+        contributors = self._repo.get_contributors()[:TOP_CONTRIBUTOR_COUNT]
+        try:
+            for contributor in contributors:
+                if contributor.company:
+                    orgs.add(_filter_name(contributor.company))
+        except Exception:
+            # Very large number of contributors, i.e. 5000+. Cap at 10.
+            return [None] * 10
+        return sorted(orgs)
+
+    @property
+    def commit_frequency(self):
+        total = 0
+        for week_stat in self._repo.get_stats_commit_activity():
+            total += week_stat.total
+        return round(total / 52, 1)
+
+    @property
+    def recent_releases(self):
+        total = 0
+        for release in self._repo.get_releases():
+            if (datetime.datetime.utcnow() -
+                    release.created_at).days > RELEASE_LOOKBACK_DAYS:
+                continue
+            total += 1
+        if not total:
+            # Make rough estimation of tags used in last year from overall
+            # project history. This query is extremely expensive, so instead
+            # do the rough calculation.
+            days_since_creation = (datetime.datetime.utcnow() -
+                                   self._repo.created_at).days
+            if not days_since_creation:
+                return 0
+            total_tags = self._repo.get_tags().totalCount
+            total = round(
+                (total_tags / days_since_creation) * RELEASE_LOOKBACK_DAYS)
+        return total
+
+    @property
+    def updated_issues(self):
+        issues_since_time = datetime.datetime.utcnow() - datetime.timedelta(
+            days=ISSUE_LOOKBACK_DAYS)
+        return self._repo.get_issues(state='all',
+                                     since=issues_since_time).totalCount
+
+    @property
+    def closed_issues(self):
+        issues_since_time = datetime.datetime.utcnow() - datetime.timedelta(
+            days=ISSUE_LOOKBACK_DAYS)
+        return self._repo.get_issues(state='closed',
+                                     since=issues_since_time).totalCount
+
+    @property
+    def comment_frequency(self):
+        issues_since_time = datetime.datetime.utcnow() - datetime.timedelta(
+            days=ISSUE_LOOKBACK_DAYS)
+        issue_count = self._repo.get_issues(state='all',
+                                            since=issues_since_time).totalCount
+        if not issue_count:
+            return 0
+        comment_count = self._repo.get_issues_comments(
+            since=issues_since_time).totalCount
+        return round(comment_count / issue_count, 1)
+
+    def get_dependents(self):
+        repo_name = self.url.replace('https://github.com/', '')
+        dependents_url = (
+            'https://github.com/search?q="{search}"&type=commits'.format(
+                search=repo_name))
+        content = ''
+        for _ in range(3):
+            result = requests.get(dependents_url)
+            if result.status_code == 200:
+                content = result.content
+                break
+            time.sleep(10)
+        match = DEPENDENTS_REGEX.match(content)
+        if not match:
+            return 0
+        return int(match.group(1).replace(b',', b''))
+
+
+def pause_if_rate_limit_exceeded(github_api):
+    rate_limit = github_api.get_rate_limit()
+    if rate_limit.core.remaining < 100:  # Conservative to avoid exceptions.
+        time.sleep(60 * 60)
+
+
+def get_param_score(param, max_value, weight=1):
+    return (math.log(1 + param) / math.log(1 + max(param, max_value))) * weight
+
+
+def get_repository_stats(repo):
+    """Return repository stats, including criticality score."""
+    created_since = repo.created_since
+    updated_since = repo.updated_since
+    contributor_count = repo.contributors
+    org_count = len(repo.get_contributor_orgs())
+    commit_frequency = repo.commit_frequency
+    recent_releases_count = repo.recent_releases
+    updated_issues_count = repo.updated_issues
+    closed_issues_count = repo.closed_issues
+    comment_frequency = repo.comment_frequency
+    dependents_count = repo.get_dependents()
+
+    total_weight = (CREATED_SINCE_WEIGHT + UPDATED_SINCE_WEIGHT +
+                    CONTRIBUTOR_COUNT_WEIGHT + ORG_COUNT_WEIGHT +
+                    COMMIT_FREQUENCY_WEIGHT + RECENT_RELEASES_WEIGHT +
+                    CLOSED_ISSUES_WEIGHT + UPDATED_ISSUES_WEIGHT +
+                    COMMENT_FREQUENCY_WEIGHT + DEPENDENTS_COUNT_WEIGHT)
+
+    criticality_score = round(
+        (get_param_score(created_since, CREATED_SINCE_THRESHOLD,
+                         CREATED_SINCE_WEIGHT) +
+         get_param_score(updated_since, UPDATED_SINCE_THRESHOLD,
+                         UPDATED_SINCE_WEIGHT) +
+         get_param_score(contributor_count, CONTRIBUTOR_COUNT_THRESHOLD,
+                         CONTRIBUTOR_COUNT_WEIGHT) +
+         get_param_score(org_count, ORG_COUNT_THRESHOLD, ORG_COUNT_WEIGHT) +
+         get_param_score(commit_frequency, COMMIT_FREQUENCY_THRESHOLD,
+                         COMMIT_FREQUENCY_WEIGHT) +
+         get_param_score(recent_releases_count, RECENT_RELEASES_THRESHOLD,
+                         RECENT_RELEASES_WEIGHT) +
+         get_param_score(closed_issues_count, CLOSED_ISSUES_THRESHOLD,
+                         CLOSED_ISSUES_WEIGHT) +
+         get_param_score(updated_issues_count, UPDATED_ISSUES_THRESHOLD,
+                         UPDATED_ISSUES_WEIGHT) +
+         get_param_score(comment_frequency, COMMENT_FREQUENCY_THRESHOLD,
+                         COMMENT_FREQUENCY_WEIGHT) +
+         get_param_score(dependents_count, DEPENDENTS_COUNT_THRESHOLD,
+                         DEPENDENTS_COUNT_WEIGHT)) / total_weight, 5)
+
+    return {
+        'name': repo.name,
+        'url': repo.url,
+        'language': repo.language,
+        'created_since': created_since,
+        'updated_since': updated_since,
+        'contributor_count': contributor_count,
+        'org_count': org_count,
+        'commit_frequency': commit_frequency,
+        'recent_releases_count': recent_releases_count,
+        'closed_issues_count': closed_issues_count,
+        'updated_issues_count': updated_issues_count,
+        'comment_frequency': comment_frequency,
+        'dependents_count': dependents_count,
+        'criticality_score': criticality_score,
+    }
+
+
+def get_repository(url):
+    """Return repository object, given a url."""
+    if not '://' in url:
+        url = 'https://' + url
+    parsed_url = urllib.parse.urlparse(url)
+    if parsed_url.netloc.endswith('github.com'):
+        auth_token = os.getenv('GITHUB_AUTH_TOKEN')
+        g = github.Github(auth_token)
+        repo_url = parsed_url.path.strip('/')
+        repo = GitHubRepository(g.get_repo(repo_url))
+        return repo
+    raise Exception('Unsupported url!')
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Gives criticality score for an open source project')
+    parser.add_argument("--repo",
+                        type=str,
+                        required=True,
+                        help="repository url")
+    parser.add_argument(
+        "--format",
+        type=str,
+        default='default',
+        choices=['default', 'csv', 'json'],
+        help="output format. allowed values are [default, csv, json]")
+
+    args = parser.parse_args()
+    r = get_repository(args.repo)
+    output = get_repository_stats(r)
+    if args.format == 'default':
+        for key, value in output.items():
+            print(f'{key}: {value}')
+    elif args.format == 'json':
+        print(json.dumps(output, indent=4))
+    elif args.format == 'csv':
+        csv_writer = csv.writer(sys.stdout)
+        csv_writer.writerow(output.keys())
+        csv_writer.writerow(output.values())
+    else:
+        raise Exception(
+            'Wrong format argument, use one of default, csv or json!')
+
+
+if __name__ == "__main__":
+    main()
