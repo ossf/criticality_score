1cc9cf27efcc1b88a55d1b69a1aa572cac5b8b73
Arnaud J Le Hors <lehors@us.ibm.com>
2022-03-30 21:39:22 +0200

    Allow overriding default parameters
    Adds a command line parameter to override the weight and threshold
    of the build-in parameters.
    
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

--- criticality_score/defaults.py
+++ criticality_score/defaults.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Constants used in OSS criticality score calculation."""
+"""Defaults used in OSS criticality score calculation."""
 import re
 
 # Weights for various parameters.
