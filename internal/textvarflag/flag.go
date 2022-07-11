a4f9ccb0bad5c4105f3d871a5e5f0dd77e19d128
Caleb Brown <calebbrown@google.com>
2022-07-12 09:38:51 +1000

    Add a backport of Go 1.19's flag.TextVar function to replace logflag.

---
+++ internal/textvarflag/flag.go
@@ -0,0 +1,55 @@
+package textvarflag
+
+import (
+	"encoding"
+	"flag"
+	"fmt"
+	"reflect"
+)
+
+// -- encoding.TextUnmarshaler Value
+type textValue struct{ p encoding.TextUnmarshaler }
+
+func newTextValue(val encoding.TextMarshaler, p encoding.TextUnmarshaler) textValue {
+	ptrVal := reflect.ValueOf(p)
+	if ptrVal.Kind() != reflect.Ptr {
+		panic("variable value type must be a pointer")
+	}
+	defVal := reflect.ValueOf(val)
+	if defVal.Kind() == reflect.Ptr {
+		defVal = defVal.Elem()
+	}
+	if defVal.Type() != ptrVal.Type().Elem() {
+		panic(fmt.Sprintf("default type does not match variable type: %v != %v", defVal.Type(), ptrVal.Type().Elem()))
+	}
+	ptrVal.Elem().Set(defVal)
+	return textValue{p}
+}
+
+func (v textValue) Set(s string) error {
+	return v.p.UnmarshalText([]byte(s))
+}
+
+func (v textValue) Get() interface{} {
+	return v.p
+}
+
+func (v textValue) String() string {
+	if m, ok := v.p.(encoding.TextMarshaler); ok {
+		if b, err := m.MarshalText(); err == nil {
+			return string(b)
+		}
+	}
+	return ""
+}
+
+// TextVar defines a flag with a specified name, default value, and usage string.
+// The argument p must be a pointer to a variable that will hold the value
+// of the flag, and p must implement encoding.TextUnmarshaler.
+// If the flag is used, the flag value will be passed to p's UnmarshalText method.
+// The type of the default value must be the same as the type of p.
+//
+// TextVar is a backport of the 1.19 implementation of flag.TextVar.
+func TextVar(fs *flag.FlagSet, p encoding.TextUnmarshaler, name string, value encoding.TextMarshaler, usage string) {
+	fs.Var(newTextValue(value, p), name, usage)
+}
