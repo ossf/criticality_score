53a5ee148929d2fdb87340c4f58577e4dd2d4a3f
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2023-01-09 12:07:06 -0600

    Included additional tests for csv
    - Inlcuded additional tests and refactored some variable names
    
    Signed-off-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>

--- internal/signalio/csv_test.go
+++ internal/signalio/csv_test.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/ossf/criticality_score/internal/collector/signal"
 )
 
 func TestMarshalValue(t *testing.T) {
@@ -78,9 +80,140 @@ func Test_csvWriter_maybeWriteHeader(t *testing.T) {
 				headerWritten: test.fields.headerWritten,
 				mu:            sync.Mutex{},
 			}
-			if err := w.maybeWriteHeader(); err != nil { // never want an error
+			if err := w.maybeWriteHeader(); err != nil { // never want an error with these test cases
 				t.Errorf("maybeWriteHeader() error = %v", err)
 			}
 		})
 	}
+}
+
+func Test_csvWriter_writeRecord(t *testing.T) {
+	type fields struct {
+		w             *csv.Writer
+		header        []string
+		headerWritten bool
+	}
+	tests := []struct { //nolint:govet
+		name    string
+		fields  fields
+		values  map[string]string
+		wantErr bool
+	}{
+		{
+			name: "write record with regular error",
+			fields: fields{
+				w: csv.NewWriter(&mockWriter{
+					written: []byte{'a', 'b', 'c'},
+					error:   nil,
+				}),
+				header:        []string{"a", "b", "c"},
+				headerWritten: true,
+			},
+			wantErr: true,
+		},
+		{
+			name: "write record with write error",
+			fields: fields{
+				w:             &csv.Writer{},
+				header:        []string{"a", "b", "c"},
+				headerWritten: true,
+			},
+			wantErr: true,
+		},
+		{
+			name: "write record with maybeWriteHeader error",
+			fields: fields{
+				w:             &csv.Writer{},
+				header:        []string{"a", "b", "c"},
+				headerWritten: false,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := &csvWriter{
+				w:             tt.fields.w,
+				header:        tt.fields.header,
+				headerWritten: tt.fields.headerWritten,
+				mu:            sync.Mutex{},
+			}
+			if err := w.writeRecord(tt.values); (err != nil) != tt.wantErr {
+				t.Errorf("writeRecord() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+type mockWriter struct { //nolint:govet
+	written []byte
+	error   error
+}
+
+func (m *mockWriter) Write(p []byte) (n int, err error) {
+	return 0, m.error
+}
+
+func Test_csvWriter_WriteSignals(t *testing.T) {
+	type args struct {
+		signals []signal.Set
+		extra   []Field
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "write signals with marshal error",
+			args: args{
+				signals: []signal.Set{
+					&testSet{
+						UpdatedCount: signal.Val(1),
+					},
+				},
+				extra: []Field{
+					{
+						Key:   "a",
+						Value: []int{1, 2, 3},
+					},
+					{
+						Key:   "b",
+						Value: map[string]string{"key": "value"},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "write signals with write error",
+			args: args{
+				extra: []Field{
+					{
+						Key:   "a",
+						Value: "1",
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			writer := CSVWriter(&mockWriter{}, []signal.Set{}, "a", "b")
+
+			if err := writer.WriteSignals(tt.args.signals, tt.args.extra...); (err != nil) != tt.wantErr {
+				t.Errorf("WriteSignals() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+type testSet struct { //nolint:govet
+	UpdatedCount signal.Field[int]
+	Field        string
+}
+
+func (t testSet) Namespace() signal.Namespace {
+	return "test"
 }
