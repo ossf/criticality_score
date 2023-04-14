d00de7430157cd0fde179f9a508e991c64889cd7
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2023-04-14 10:54:33 -0500

    Included more tests
    Signed-off-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>

--- internal/signalio/helpers_test.go
+++ internal/signalio/helpers_test.go
@@ -37,20 +37,26 @@ func Test_marshalToMap(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "valid input",
+			name: "default",
 			extra: []Field{
 				{
 					Key:   "test",
 					Value: "1",
+				},
+				{
+					Key:   "test2",
+					Value: "2",
 				},
 			},
 			signals: []signal.Set{
 				&testSet{
 					UpdatedCount: signal.Val(1),
 				},
+				&testSet{
+					UpdatedCount: signal.Val(3),
+				},
 			},
-			want:    map[string]string{"test.updated_count": "1", "test": "1"},
-			wantErr: false,
+			want: map[string]string{"test": "1", "test.updated_count": "3", "test2": "2"},
 		},
 		{
 			name: "marshal error extra",
@@ -67,6 +73,29 @@ func Test_marshalToMap(t *testing.T) {
 			},
 			want:    nil,
 			wantErr: true,
+		},
+		{
+			name: "empty extra",
+			signals: []signal.Set{
+				&testSet{
+					UpdatedCount: signal.Val(1),
+				},
+			},
+			want: map[string]string{"test.updated_count": "1"},
+		},
+		{
+			name: "empty signals",
+			extra: []Field{
+				{
+					Key:   "test",
+					Value: "1",
+				},
+			},
+			want: map[string]string{"test": "1"},
+		},
+		{
+			name: "empty signals and extra",
+			want: map[string]string{},
 		},
 	}
 	for _, test := range tests {
@@ -108,8 +137,27 @@ func Test_fieldsFromSignalSets(t *testing.T) {
 				&testSet{
 					UpdatedCount: signal.Val(1),
 				},
+				&testSet{
+					UpdatedCount: signal.Val(3),
+				},
 			},
-			want: []string{"test.updated_count", "test"},
+			want: []string{"test.updated_count", "test.updated_count", "test"},
+		},
+		{
+			name: "empty extra",
+			sets: []signal.Set{
+				&testSet{
+					UpdatedCount: signal.Val(1),
+				},
+			},
+			want: []string{"test.updated_count"},
+		},
+		{
+			name: "empty sets",
+			extra: []string{
+				"test",
+			},
+			want: []string{"test"},
 		},
 		{
 			name: "panics",
